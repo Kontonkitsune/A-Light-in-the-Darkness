f5867ade1e3e1260c64d80ef3d873c3269bb427e
Lucian <lucian.reiter@gmail.com>
2021-06-03 18:19:00 -0500

    It actually works now lol and much much better than the initial working version i might add

--- ExampleBattleScript.c
+++ ExampleBattleScript.c
@@ -7,14 +7,19 @@
 int CharacterBattleStats[2][7][20] = {
 	{ 
 		{300,300,250,250,75,125,100,75,125,100,100,0,0,100,56},
-		{500,500,100,100,150,50,75,125,75,100,80,0,0,100,43}},
+		{500,500,100,100,150,50,75,125,75,100,80,0,0,100,43},
+		{250,250,250,250,100,100,75,75,75,150,100,0,0,100,87},
+		{200,200,400,400,75,150,125,125,75,125,100,0,0,100,22},
+		{500,500,100,100,100,100,150,75,125,100,120,0,0,100,10},},
 	{
 		{400,400,0,0,75,0,0,150,75,100,80,0,0,100,98},
+		{400,400,0,0,75,0,0,150,75,100,80,0,0,100,91},
+		{400,400,0,0,75,0,0,150,75,100,80,0,0,100,62},
 		{400,400,0,0,75,0,0,150,75,100,100,20,0,100,47}}
 };
 char CharacterNames[2][7][20] = {
-	{"Nia","Ral"},
-	{"Jelly A","Jelly B"}
+	{"Nia","Ral","Fynn","Sera","Ria"},
+	{"Jelly A","Jelly B","Jelly C","Jelly D"}
 };
 int CharacterResistances[2][7][22][2] = {
 	{
@@ -37,8 +42,8 @@ int DisplayCharacterInfo[][][] = {
 };*/
 
 int leaderCharacter = 0;
-int partysize = 2;
-int enemypartysize = 2;
+int partysize = 5;
+int enemypartysize = 4;
 
 
 
@@ -72,7 +77,6 @@ int dealDamage(int targetteam, int targetcharacter, int damage) {
 };
 
 int doNormalAttack(int startteam, int startchar, int targetteam, int targetchar) {
-	int localtarget = rand() % 2;
 	if ( rand() % 100 < CharacterBattleStats[startteam][startchar][10] - CharacterBattleStats[targetteam][targetchar][11]) {
 	int localdamage = calcDamage(startteam,startchar,targetteam,targetchar);
 	printf("%s deals %d damage to %s\n", CharacterNames[startteam][startchar], localdamage, CharacterNames[targetteam][targetchar]);
@@ -83,9 +87,11 @@ int doNormalAttack(int startteam, int startchar, int targetteam, int targetchar)
 	};
 	return 0;
 };
-
-
-
+/*
+int castSkill() {
+	
+};
+*/
 
 int flag1 = 0;
 int flag2 = 0;
@@ -93,42 +99,51 @@ char input[15];
 
 int playerturn(int localcharacter) {
 	int localflag1 = 0;
+	int actionchoice = 0;
 	while(localflag1==0) {
 		printf("Spirit: %d/%d\nMana: %d/%d\n", CharacterBattleStats[0][0][0],CharacterBattleStats[0][0][1],CharacterBattleStats[0][0][2],CharacterBattleStats[0][0][3]);
-		printf("choose action: \nFight\nMagic\nItem\nDefend\nRun\nExit");
+		printf("choose action: \n1: Fight\n2: Magic\n3: Item\n4: Defend\n5: Status\n6: Run\n7: Exit");
 		printf(">>> ");
 		gets(input);
-		puts(input);
-		if(strcmp(input,"Fight") == 0) localflag1 = 1;
-		if(strcmp(input,"Item") == 0) localflag1 = 1;
-		if(strcmp(input,"Run") == 0) localflag1 = 1;
-		if(strcmp(input,"Defend") == 0) localflag1 = 1;
-		if(strcmp(input,"Status") == 0) localflag1 = 1;
-		if(strcmp(input,"Exit") == 0) localflag1 = 1;
-		if(strcmp(input,"Magic") == 0) localflag1 = 1;
+		actionchoice = atoi(input);
+		if(actionchoice > 0 && actionchoice < 8) localflag1 = 1;
 	};
 	
-	if(strcmp(input,"Fight") == 0) {
-		printf("Choose enemy:\n0-1 > ");
+	if(actionchoice == 1) {
+		printf("Choose enemy:\n1-%d > ", enemypartysize);
 		gets(input);
-		doNormalAttack(0,localcharacter,1,atoi(input));
+		doNormalAttack(0,localcharacter,1,atoi(input) - 1);
 	};
-	if(strcmp(input,"Item") == 0) {
-		printf("You have no items, stupid. you waste a turn.");
-	};
-	if(strcmp(input,"Run") == 0) {};
-	if(strcmp(input,"Defend") == 0) {};
-	if(strcmp(input,"Status") == 0) {
+	if(actionchoice == 2) {
+		printf("Not implemented.");
 		
 	};
-	if(strcmp(input,"Exit") == 0) flag2 = 1;
+	if(actionchoice == 3) {
+		printf("You have no items, stupid. you waste a turn.");
+	};
+	if(actionchoice == 4) {
+		printf("Not implemented.");
+	};
+	if(actionchoice == 5) {
+		printf("Not implemented.");
+	};
+	if(actionchoice == 6) {
+		printf("Not implemented.");
+	};
+	if(actionchoice == 7) flag2 = 1;
 	return 0;
 }
 
 int aiturn(int localteam, int localcharacter) {
 	int localaction = rand() % 20;
 	if(localaction < 30) {
-		int localtarget = rand() % 2;
+		int localtarget;
+		if (localteam == 0) {
+			localtarget = rand() % enemypartysize;
+		}
+		else {
+			localtarget = rand() % partysize;
+		}
 		doNormalAttack(localteam,localcharacter,1 - localteam,localtarget);
 	}
 	return 0;
@@ -141,20 +156,21 @@ int battleScript() {
 	int teamid = 0;
 	while (battleflag == 0) {
 		battleTick += 1;
-		for (int i; i < partysize + enemypartysize; i += 1) {
+		for (int i = 0; i+1 < partysize + enemypartysize; i += 1) {
 			if (i >= partysize) {
 				tempid = i - partysize;
 				teamid = 1;
 			}
 			else {
 				teamid = 0;
+				tempid = i;
 			}; //battletick mod charspeed - initiative 1400 % 125 - 56 = 
-			if ((battleTick % (int)(100 / CharacterBattleStats[teamid][i][9]) - CharacterBattleStats[teamid][i][14]) == 0) {
-				if(teamid = 0 && leaderCharacter == i ) {
+			if ((battleTick % (int)(10000 / CharacterBattleStats[teamid][tempid][9]) - CharacterBattleStats[teamid][tempid][14]) == 0) {
+				if(leaderCharacter == i ) {
 					playerturn(i);
 				}
 				else {
-					aiturn(teamid,i);
+					aiturn(teamid,tempid);
 				};
 			};
 			
